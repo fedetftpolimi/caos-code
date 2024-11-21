f29d70e9778a00e336f2d6af806c278d0b0826cb
Simone Perriello <8199216+tigerjack@users.noreply.github.com>
2024-11-21 19:08:54 +0100

    ADD new c functions

---
+++ exercises/c/fibonacci.c
@@ -0,0 +1,52 @@
+/*
+ * Write a program that, given an integer $N > 0$ as input, prints the
+ * first $N$ Fibonacci numbers.
+ * 
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+  // Dichiariamo e acquisiamo il nostro N
+  unsigned n;
+  printf("Insert a positive number\n");
+  scanf("%u", &n);
+
+  // Check if the value is greater than 0
+  if (n <= 0) {
+    printf("The value should be strictly positive!");
+    return 0;
+  }
+
+  int prev1, prev2, cur;
+
+  // Print first number
+  printf("0 ");
+
+  // If we only wanted the first number, we can return
+  if (n == 1) {
+    return 0;
+  }
+
+  // second number
+  printf("1 ");
+
+  // in slides, prev1=a, prev2=b, cur=c
+  prev1 = 0;
+  prev2 = 1;
+
+  // The loop will be executed n - 2 times, since we already printed the first two numbers
+  while (n > 2) {
+    // Compute n-th number in fibonacci series
+    cur = prev1 + prev2;
+    printf("%u ", cur);
+
+    // Update the variables 
+    prev1 = prev2;
+    prev2 = cur;
+    n--;
+  }
+  printf("\n");
+
+  return 0;
+}
